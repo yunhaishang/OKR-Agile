f191bb227491fd14d9bf6ac1757edbc9cb5f07c7
liuxiaoyun <1194544085@qq.com>
2026-01-04 22:37:13 +0800

    add swagger and cors

--- src/main/java/com/se/okr_agile/config/WebConfig.java
+++ src/main/java/com/se/okr_agile/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.se.okr_agile.config;
 import com.se.okr_agile.interceptor.JwtInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,6 +19,17 @@ public class WebConfig implements WebMvcConfigurer {
                 .addPathPatterns("/**")          // 拦截所有请求
                 .excludePathPatterns("/auth/register", // 排除注册,登录,swagger文档
                         "/auth/login",
-                        "/swagger-ui/**");
+                        "/swagger-ui/**",
+                        "/v3/api-docs/swagger-config",
+                        "/v3/api-docs/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 匹配所有 API 路径
+                .allowedOriginPatterns("http://localhost:5173") // 允许的前端地址
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
